1985a9544f3d35fec5db2271781f4bcc7fefd8f4
NiloofarSafari <aniksafari9033@gmail.com>
2022-10-12 11:55:47 -0400

    firstcommit

---
+++ src/test/java/assignmentoct9/browser2.java
@@ -0,0 +1,29 @@
+package assignmentoct9;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class browser2 {
+
+	public static void main(String[] args) {
+		
+		
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://costco.com");
+		driver.manage().window().maximize();
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver1 = new ChromeDriver();
+		driver1.get("https://walmart.com");
+		driver1.manage().window().maximize();
+		
+		driver.quit();
+		driver1.quit();
+		
+	}
+
+}
